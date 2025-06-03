e2caed776ce0963d8f3086e1af5ef289ce622323
Mester0723 <postmatoric@gmail.com>
2025-06-03 10:33:10 +0500

    New Commands!

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from logic import Pokemon
 
 bot = telebot.TeleBot(token)
 
+# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "👋 Привет, добро пожаловать в генератор покемонов!\n"
                                       "📃 Я бот для создания покемонов.\n"
                                       "👇 Напиши /go, чтобы создать покемона.")
 
-# Обработчик команды /go или /start
+# Обработчик команды /go
 @bot.message_handler(commands=['go'])
 def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
@@ -20,10 +21,64 @@ def go(message):
     else:
         bot.reply_to(message, "🚫 Ты уже создал себе покемона")
 
+# Кормление покемона
+@bot.message_handler(commands=['feed'])
+def feed(message):
+    username = message.from_user.username
+    if username in Pokemon.pokemons:
+        pokemon = Pokemon.pokemons[username]
+        result = pokemon.feed()
+        bot.send_message(message.chat.id, result)
+    else:
+        bot.send_message(message.chat.id, "🚫 Сначала создай покемона с помощью команды /go")
+
+# Достижения покемона
+@bot.message_handler(commands=['achievements'])
+def achievements(message):
+    username = message.from_user.username
+    if username in Pokemon.pokemons:
+        pokemon = Pokemon.pokemons[username]
+        ach = pokemon.get_achievements()
+        if ach:
+            bot.send_message(message.chat.id, "🏆 Твои достижения:\n" + "\n".join(ach))
+        else:
+            bot.send_message(message.chat.id, "🔍 Пока нет достижений.\n"
+                                              "🍔 Кормите и развивайте покемона!")
+    else:
+        bot.send_message(message.chat.id, "🚫 Сначала создай покемона с помощью команды /go")
+
+# Уровень и опыт покемона
+@bot.message_handler(commands=['level'])
+def level(message):
+    username = message.from_user.username
+    if username in Pokemon.pokemons:
+        pokemon = Pokemon.pokemons[username]
+        bot.send_message(
+            message.chat.id,
+            f"🔝 Уровень покемона {pokemon.name}: {pokemon.level}\n"
+            f"➰ Опыт: {pokemon.exp}"
+        )
+    else:
+        bot.send_message(message.chat.id, "🚫 Сначала создай покемона с помощью команды /go")
+
+# Информация о покемоне
+@bot.message_handler(commands=['info'])
+def info(message):
+    username = message.from_user.username
+    if username in Pokemon.pokemons:
+        pokemon = Pokemon.pokemons[username]
+        bot.send_message(message.chat.id, pokemon.info())
+    else:
+        bot.send_message(message.chat.id, "🚫 Сначала создай покемона с помощью команды /go")
+
+# Реакция на любые сообщения
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.send_message(message.chat.id, " Извини, если тебе не понятно, загляни в список командв нижнем-левом углу.")
+
+# Секретная команда
 @bot.message_handler(commands=['ilikechocolatesomuchandilikesecretcommandsbecauseitiscool'])
 def secret(message):
-    bot.send_message(message.chat.id, "Слишком длинная команда, хватит тратить кучу времени на её написание, этого ведь ничего не даст!\n"
-                                      "Ах! Беру свои слова обратно! Потому что любишь шоколад, кто его может не любить? Это же прекрастное дополнение для десертов!\n"
-                                      "Секретные команды? Что за чужь! Тебе лишь бы занятся бесполезной чепухой!? Тьфу!!!")
+    bot.send_message(message.chat.id, "TypeError: This command is not allowed yet, but Chocolate is useful for you.")
 
 bot.infinity_polling(none_stop=True)
